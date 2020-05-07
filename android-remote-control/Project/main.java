2855818b74b515b17cb4beed0fcb40a71f79c0a6
Pavel Levshic <pavellevshic@gmail.com>
2020-05-07 15:58:34 +0300

    Update main.java

--- android-remote-control/Project/main.java
+++ android-remote-control/Project/main.java
@@ -1,1 +1,27 @@
+package com.example.testapp;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Button button = new Button(this);
+        button.setText("Big button");
+
+        button.setOnClickListener(v -> {
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("From lambda")
+                    .setMessage(getString(R.string.hello))
+                    .show();
+        });
+        
+        setContentView(button);
+    }
+}
